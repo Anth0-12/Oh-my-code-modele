a731be5a8124796553b8100d787b3ec0080d14aa
Anthony Hecquet <47087871+Anth0-12@users.noreply.github.com>
2024-12-10 21:20:02 +0100

    Add files via upload

--- initiation/script.js
+++ initiation/script.js
@@ -1,5 +1,7 @@
 // Je sélectionne et stock
 const btnRed = document.getElementById("btn-red");
+const btnBlue = document.getElementById("btn-blue");
+
 let compteur = 0;
 console.log(compteur, "compteur initial");
 
@@ -9,7 +11,21 @@ btnRed.addEventListener("click", function () {
 
   // Ici l'action
 
-  // Incrémenter le compteur de 1
+  // Incrémenter le compteur de 1 avec la fonction
+  add();
+});
+
+// Je soumets le cercle bleu à une action
+btnBlue.addEventListener("click", function () {
+  console.log("Clique sur le cercle rouge");
+
+  // Ici l'action
+
+  // Incrémenter le compteur de 1 avec la fonction
+  add();
+});
+
+function add() {
   compteur = compteur + 1;
   console.log(compteur, "compteur incrementé");
-});
+}
