9c74bc2b990e4dfb8d4e6612ad45e46c236463f6
Anthony Hecquet <47087871+Anth0-12@users.noreply.github.com>
2024-12-20 00:23:49 +0100

    Add files via upload

--- dark_mode/script.js
+++ dark_mode/script.js
@@ -1,14 +1,9 @@
 // Je séléctionne et stocke
 const switchBox = document.querySelector('.switch');
-console.log(switchBox);
 const btn = document.querySelector('.btn');
-console.log(btn);
 const icone = document.querySelector('i');
-console.log(icone);
 const container = document.querySelector('.container');
-console.log(container);
 const titre = document.querySelector('h1');
-console.log(titre);
 
 // Je soumets la div switch à une action au clic
 switchBox.addEventListener('click', function() {
@@ -19,4 +14,15 @@ switchBox.addEventListener('click', function() {
     // Je change l'icone
     icone.classList.toggle('fa-moon'); // Je désactive la classe "fa-moon"
     icone.classList.toggle('fa-sun'); // J'ajoute et active la classe "fa-sun"
+    // Je change la couleur de fond du container
+    container.classList.toggle('container-change');
+    // Je change la couleur de fond du switch
+    titre.classList.toggle('titre-change');
+
+    // Je modifie le texte du titre
+    if (titre.innerText === "Dark mode") {
+        titre.innerText = "Light mode";
+    } else {
+        titre.innerText = "Dark mode";
+    }
 });
