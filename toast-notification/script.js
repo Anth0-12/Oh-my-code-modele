cfec4186b2971d72214d1dfba0758c7535a7e244
Anthony Hecquet <47087871+Anth0-12@users.noreply.github.com>
2025-01-03 04:21:01 +0100

    Add files via upload

---
+++ toast-notification/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('.btn');
+const toast = document.querySelector('.toast');
+
+btn.addEventListener('click', function() {
+    const notification = document.createElement('div');
+    notification.innerText = 'Votre message a bien été envoyé !';
+    toast.appendChild(notification);
+    notification.classList.add('active');
+    console.log('toast');
+    setTimeout(() => {
+        notification.classList.add('transition');
+        
+        // Attendre la fin de l'animation avant de supprimer l'élément
+            setTimeout(() => {
+                notification.remove();    
+            }, 500);  // Temps de l'animation de disparition
+
+    }, 500); // L'animation commence après 1 seconde
+});
